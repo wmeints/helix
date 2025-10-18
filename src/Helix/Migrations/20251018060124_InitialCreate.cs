d396b8f946d98dabbb4e2e837b44432adc25610f
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-10-18 08:04:18 +0200

    Fix migration files

---
+++ src/Helix/Migrations/20251018060124_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Helix.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conversations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Topic = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    ChatHistory = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PendingFunctionCalls = table.Column<string>(type: "TEXT", nullable: false),
+                    ConcurrencyToken = table.Column<byte[]>(type: "BLOB", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conversations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conversations");
+        }
+    }
+}
